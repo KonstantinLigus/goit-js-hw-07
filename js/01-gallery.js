2c91ff92f172564109f2dfb6a25417bfa68e2599
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-22 21:43:58 +0300

    +1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryRender = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<img src="${preview}" data-src="${original}" alt="${description}">`
+  )
+  .join("");
+
+gallery.innerHTML = galleryRender;
+
+gallery.addEventListener("click", onImgClic);
+
+function onImgClic(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  // event.target.src = event.target.dataset.src;
+  event.target.onclick = () => {
+    basicLightbox
+      .create(
+        `<img src="${original}" data-src="${original}" alt="${description}">`
+      )
+      .show();
+  };
+}
+// console.log(gallery);
