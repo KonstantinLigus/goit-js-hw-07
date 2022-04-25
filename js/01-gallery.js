7aaa7b685453f7d7d5dd3ee9aae0a31bc8811341
Konstantin Ligus <kostya1989com@gmail.com>
2022-04-25 12:46:16 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,7 +8,16 @@ function galleryRender(galleryItems) {
   refs.gallery.innerHTML = galleryItems
     .map(
       ({ preview, original, description }) =>
-        `<div class="gallery__item"><a class="gallery__link" href="${original}"><img src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+          class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
     )
     .join("");
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+function galleryRender(galleryItems) {
+  refs.gallery.innerHTML = galleryItems.reduce(
+    (acc, { preview, original, description } = galleryItems) =>
+      acc +
+      `<a class="gallery__item" href=${original}>
+<img class="gallery__image" src="${preview}" alt=${description} />
+</a>`,
+    ""
+  );
+}
+galleryRender(galleryItems);
+console.log(refs.gallery);
